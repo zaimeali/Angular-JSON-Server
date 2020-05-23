a4289feef929be9c1d9e61e2e1f3d8b9a21ae46c
Zaime Ali <zaimeali1997@gmail.com>
2020-05-23 21:31:41 +0500

    FrontEnd Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-json-app';
+
+  addUser(form: NgForm){
+    console.log(form);
+  }
 }
